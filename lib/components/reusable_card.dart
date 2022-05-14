8205f88c705e26a749191b016ac2730c0d1cb0e9
Navpreet Singh <navpreetsingh951818@gmail.com>
2022-05-14 16:33:54 +0530

    init

---
+++ lib/components/reusable_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard(
+      {required this.colour, required this.cardChild, required this.onPress});
+
+  final Color colour;
+  final Widget cardChild;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onPress();
+      },
+      child: Container(
+        margin: const EdgeInsets.all(5.0),
+        padding: const EdgeInsets.all(2),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: cardChild,
+      ),
+    );
+  }
+}
